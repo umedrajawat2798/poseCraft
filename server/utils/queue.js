db4418f06a6fc8652fe0418d874ca479096bf453
umedrajawat2798 <umedrajawat271998@gmail.com>
2024-07-09 00:27:12 +0530

    Update env queue.js

--- server/utils/queue.js
+++ server/utils/queue.js
@@ -1,11 +1,19 @@
 const Bull = require('bull');
 
+require('dotenv').config();
+
 const imageQueue = new Bull('image-processing', {
   redis: {
-    host: '127.0.0.1',
-    port: 6379
+    host: process.env.REDIS_HOST || "127.0.0.1",
+    port: process.env.PORT || 6379,
   }
 });
+
+imageQueue.isReady().then(()=>{
+  console.log("bull connected")
+}).catch((err)=> {
+  console.log("error in bull", err)
+})
 
 module.exports = imageQueue;
 
