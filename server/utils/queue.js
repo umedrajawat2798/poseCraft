8757f4d8e05125e2a4474e9cc22db9f731e85514
umedrajawat2798 <umedrajawat271998@gmail.com>
2024-07-09 13:19:58 +0530

    Update queue.js

--- server/utils/queue.js
+++ server/utils/queue.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const imageQueue = new Bull('image-processing', {
   redis: {
     host: process.env.REDIS_HOST || "127.0.0.1",
-    port: process.env.PORT || 6379,
+    port: process.env.REDIS_PORT || 6379,
   }
 });
 
